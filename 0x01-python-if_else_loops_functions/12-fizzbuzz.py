0b1e3b011e3526685464fe69534456952616f455
Olawole Olawale Martins <olawulzy@yahoo.co.uk>
2023-06-06 15:21:34 +0100

    editted task 12 - firstline shebang added

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,14 +2,11 @@
 def fizzbuzz():
     for _ in range(1, 101):
         if _ % 15 == 0:
-            print("FizzBuzz", end="")
+            print("FizzBuzz", end=" ")
         elif _ % 5 == 0:
-            print("Buzz", end="")
+            print("Buzz", end=" ")
         elif _ % 3 == 0:
-            print("Fizz", end="")
+            print("Fizz", end=" ")
         else:
-            print(_, end="")
-        if _ == 100:
-            print("")
-        else:
-            print(" ", end="")
+            print(_, end=" ")
+       
