84045cb17ae90197370d2bc79e2550bdeb375d87
Olawole Olawale Martins <olawulzy@yahoo.co.uk>
2023-06-08 12:40:52 +0100

    task 3 editted added guard not to run on import

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
-sum = 0
-for i, _ in enumerate(argv):
-    if i != 0:
-        sum += int(_)
-print(sum)
+if __name__ == "__main__":
+    from sys import argv
+    sum = 0
+    for i, _ in enumerate(argv):
+        if i != 0:
+            sum += int(_)
+    print(sum)
