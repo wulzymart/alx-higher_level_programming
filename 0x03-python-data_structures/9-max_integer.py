94e4be2d52204be4f26ddffd706655d0b8424bbb
Olawole Olawale Martins <olawulzy@yahoo.co.uk>
2023-06-10 09:32:23 +0100

    Task 3 editted, added if guard for empty lists

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = None if not len(my_list) else my_list[1]
+    max = None if not my_list else my_list[1]
     if my_list:
         for _ in my_list:
             max = _ if _ > max else max
