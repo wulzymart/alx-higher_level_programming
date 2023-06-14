9a3018025dac2b4ba819f0b2fd1baac469b9a462
Olawole Olawale Martins <olawulzy@yahoo.co.uk>
2023-06-14 17:13:42 +0100

    Tasks 1 edited, now returns list instead of list containing list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if my_list:
-        new_list = [list(map(lambda x: replace if x == search else x,
-                             my_list))]
+        new_list = list(map(lambda x: replace if x == search else x,
+                             my_list))
         return new_list
     return []
