069979ae84198c789dfff86de4344d847ca38511
Olawole Olawale Martins <olawulzy@yahoo.co.uk>
2023-06-14 17:15:01 +0100

    Tasks 1 edited, fixed indentation

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     if my_list:
         new_list = list(map(lambda x: replace if x == search else x,
-                             my_list))
+                            my_list))
         return new_list
     return []
