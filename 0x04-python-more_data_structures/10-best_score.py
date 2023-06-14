7c6e0460bb28f6e8fc546d54fa391d369a6b8e70
Olawole Olawale Martins <olawulzy@yahoo.co.uk>
2023-06-14 17:10:18 +0100

    Tasks 10 editted, code now checks for previous highest value instead of last value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,9 @@ def best_score(a_dictionary):
     best = None
     innital = 0
     if a_dictionary:
+        best_val = list(a_dictionary.values())[0]
         for key, value in a_dictionary.items():
-            best = key if value >= innital else best
-            innital = value
+            if value >= best_val:
+                best = key
+                best_val = value
     return best
