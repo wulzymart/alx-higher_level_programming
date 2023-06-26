47f3ae76e414ed6c41877aa44ee713126a0c19e6
Olawole Olawale Martins <olawalemart@gmail.com>
2023-06-26 13:51:27 +0000

    Tasks 0 and 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if my_list:
-        try:
-            i = 0
-            for _ in range(x):
-                print("{}".format(my_list[_]), end='')
-                i += 1
-        except Exception as exception:
-            pass
-        print("")
-        return i
-    return 0
+    try:
+        i = 0
+        for _ in range(x):
+            print("{}".format(my_list[_]), end='')
+            i += 1
+    except Exception as exception:
+        pass
+    print("")
+    return i
