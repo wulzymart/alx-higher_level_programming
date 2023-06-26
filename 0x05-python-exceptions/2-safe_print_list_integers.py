3e7f7d5cba6dba1840bfbfb26421c51ea8ffb026
Olawole Olawale Martins <olawalemart@gmail.com>
2023-06-26 15:04:17 +0000

    Tasks 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,22 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[_]), end='')
             i += 1
-        except IndexError:
-            raise
-        except Exception as exception:
+        except (TypeError, ValueError):
             pass
     print("")
     return i
-
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
