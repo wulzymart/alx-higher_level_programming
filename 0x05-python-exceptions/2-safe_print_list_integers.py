28d63389b2d9756724403022ed54367e713982ea
Olawole Olawale Martins <olawalemart@gmail.com>
2023-06-26 14:13:36 +0000

    Tasks 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[_]), end='')
             i += 1
-        except (ValueError, TypeError):
+        except IndexError:
+            raise
+        except Exception as exception:
             pass
     print("")
     return i
