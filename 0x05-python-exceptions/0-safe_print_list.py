3a0c40378801d224e33fa61276527c6275c96def
Olawole Olawale Martins <olawalemart@gmail.com>
2023-06-26 13:31:18 +0000

    Tasks 0 and 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    if my_list:
+        try:
+            i = 0
+            for _ in range(x):
+                print(my_list[_], end='')
+                i += 1
+        except IndexError:
+            pass
+        print("")
+        return i
