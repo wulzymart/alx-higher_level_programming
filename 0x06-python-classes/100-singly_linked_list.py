40f43b57df80e5a498c1c2da58890d808bcb68b0
OlawoleMartins <olawalemart@gmail.com>
2023-06-27 21:48:20 +0100

    tasks 7

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""Defines class for node and singly linked list"""
+
+
+class Node:
+    """The node of a singly linked list"""
+    def __init__(self, data, next_node=None):
+        """initializes a the node of a singly linked list
+        Arguements:
+        data: data in the node
+        next_node: the next node in the list
+        """
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """gets private attribute 'data'"""
+        return self.__data
+
+    @data.setter
+    def data(self, data):
+        """sets private attribute 'data'"""
+        if isinstance(data, int) or data is None:
+            self.__data = data
+        else:
+            raise TypeError("data must be an integer")
+
+    @property
+    def next_node(self):
+        """gets private attribute 'next_node'"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Sets private attribute 'next_node'"""
+        if isinstance(value, Node) or value is None:
+            self.__next_node = value
+        else:
+            raise TypeError("next_node must be a Node object")
+
+
+class SinglyLinkedList:
+    """Class for a singly linked list of integers"""
+    def __init__(self):
+        """Initializes the singly linked list head node"""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """Inserts a new node into the list in a sorted manner"""
+        node = Node(value)
+        node.next_node = None
+
+        if not self.__head:
+            self.__head = node
+            return
+        head = self.__head
+        if self.__head.data > value:
+            node.next_node = self.__head
+            self.__head = node
+            return
+        prev = head
+        while head:
+            if head.data > value:
+                node.next_node = head
+                prev.next_node = node
+                return
+            prev = head
+            head = head.next_node
+        prev.next_node = node
+
+    def __str__(self):
+        datas = []
+        head = self.__head
+        while head:
+            datas += [str(head.data)]
+            head = head.next_node
+        return "\n".join(datas)
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -95,3 +95,27 @@ Write a class Square that defines a square by: (based on 5-square.py)
     	if size is equal to 0, print an empty line
     	position should be use by using space - Don’t fill lines by spaces when position[1] > 0
     You are not allowed to import any module
+
+# Task 7:
+
+Write a class Node that defines a node of a singly linked list by:
+
+    Private instance attribute: data:
+    	property def data(self): to retrieve it
+    	property setter def data(self, value): to set it:
+    		data must be an integer, otherwise raise a TypeError exception with the message data must be an integer
+    Private instance attribute: next_node:
+    	property def next_node(self): to retrieve it
+    	property setter def next_node(self, value): to set it:
+    		next_node can be None or must be a Node, otherwise raise a TypeError exception with the message next_node must be a Node object
+    Instantiation with data and next_node: def **init**(self, data, next_node=None):
+
+And, write a class SinglyLinkedList that defines a singly linked list by:
+
+    Private instance attribute: head (no setter or getter)
+    Simple instantiation: def **init**(self):
+    	Should be printable:
+    	print the entire list in stdout
+    one node number by line
+    Public instance method: def sorted_insert(self, value): that inserts a new Node into the correct sorted position in the list (increasing order)
+    You are not allowed to import any module
