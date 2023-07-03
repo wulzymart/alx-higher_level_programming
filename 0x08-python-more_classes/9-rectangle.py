24c7961ad6ec8957febe3c0110f0579293add5b3
OlawoleMartins <olawalemart@gmail.com>
2023-07-03 17:13:57 +0100

    tasks 9 editted

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -90,7 +90,7 @@ class Rectangle:
 
     def __repr__(self):
         """returns official representation of the instance"""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        return "Rectangle(" + str(self.__width) + ", " + str(self.__height) + ")"
 
     def __del__(self):
         """Called when instance is deleted"""
