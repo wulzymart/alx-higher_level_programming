0d52c607f7888761cda627747999fb2dcc9a5466
OlawoleMartins <olawalemart@gmail.com>
2023-07-04 11:07:58 +0100

    All tasks

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module containing class lockedclass"""
+
+
+class LockedClass(object):
+    """Empty class that cannot have new attribute set except first_name"""
+    def __setattr__(self, key, value):
+        if key != "first_name":
+            raise AttributeError(f"'LockedClass' object has no attribute '{key}'")
+        object.__setattr__(self, key, value)
