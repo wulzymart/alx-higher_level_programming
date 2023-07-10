a03657e64f8323b665de20cbb40d2478a77a6210
Olawole Olawale Martins <olawalemart@gmail.com>
2023-07-10 17:24:10 +0100

    mandatory tasks

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,9 +4,6 @@
 
 class MyList(list):
     """A subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
 
     def print_sorted(self):
         """prints sorted list in ascending order
