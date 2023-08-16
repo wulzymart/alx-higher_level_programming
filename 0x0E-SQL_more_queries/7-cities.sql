35e316bc62f3180c257bfa721c2881b5d6241bd5
Olawole Olawale Martins <olawalemart@gmail.com>
2023-08-17 00:19:54 +0100

    task 7 edit

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,11 +5,14 @@
 -- name VARCHAR(256) can’t be null
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table cities already exists, your script should not fail
+-- create database
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+-- select database
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+-- create table
+CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL
-    FOREIGN KEY REFERENCES `hbtn_0d_usa`.`states`
+    FOREIGN KEY(state_id) REFERENCES states.ìd
 );
