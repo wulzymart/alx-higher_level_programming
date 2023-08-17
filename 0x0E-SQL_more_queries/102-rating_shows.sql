7aa60981e6318d70de921aa5cb68351b86dece2a
Olawole Olawale Martins <olawalemart@gmail.com>
2023-08-17 01:10:18 +0100

    102 editted all keywords in upper case

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,7 +2,7 @@
 -- Each record should display: tv_shows.title - rating sum
 -- Results must be sorted in descending order by the rating
 -- You can use only one SELECT statement
-SELECT tv_shows.title as title, SUM(tv_show_ratings.rate) AS rating
+SELECT tv_shows.title AS title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows INNER JOIN tv_show_ratings
 ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY title
