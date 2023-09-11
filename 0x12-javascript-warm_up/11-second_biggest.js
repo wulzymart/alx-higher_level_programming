590b440cba0bf0e1f17dd82b84bc7cd56c00562c
Olawole Olawale Martins <olawalemart@gmail.com>
2023-09-11 21:43:53 +0100

    task 11 corrected

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2, process.argv.length);
 let sec;
-if (args.length <= 3) console.log(0);
+if (args.length <= 1) console.log(0);
 else {
   sec = args.map(arg => parseInt(arg)).sort((a, b) => b - a)[1];
   console.log(sec);
