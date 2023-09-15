620cb4a80580025acf84f057592aee2a5bac329b
Olawole Olawale Martins <olawalemart@gmail.com>
2023-09-16 00:40:19 +0100

    up to task 9 ensure ports

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name)
+        print('{0}: {1}'.format(instance.id, instance.name))
