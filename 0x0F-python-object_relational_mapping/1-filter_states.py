2e95ba0660fc045596e53f22cf5878774e1029b0
Olawole Olawale Martins <olawalemart@gmail.com>
2023-09-15 15:37:15 +0100

    0 - 2

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that lists all states starting with 'N'
+from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv as args
+
+if __name__ == "__main__":
+    """connect to database and execute query"""
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=args[1], passwd=args[2], db=args[3])
+    cur = db.cursor()
+    cur.execute(
+        "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that lists all states entered by user from the
+database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv as args
+
+if __name__ == "__main__":
+    """connect to database and execute query"""
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=args[1], passwd=args[2], db=args[3])
+    cur = db.cursor()
+    cur.execute(
+        "SELECT id, name FROM states WHERE name = '{}' ORDER BY id ASC"
+        .format(args[4]))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
