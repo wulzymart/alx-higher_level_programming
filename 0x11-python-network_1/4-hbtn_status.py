34fd0ee0ef55ed320728a1f02cc80bedd47791d1
wulzymart <72075175+wulzymart@users.noreply.github.com>
2023-09-29 17:17:35 +0000

    fixed typos

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,6 +7,6 @@ import requests
 
 if __name__ == "__main__":
     res = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response")
+    print("Body response:")
     print("\t- type: {}".format(type(res.text)))
     print("\t- content: {}".format(res.text))
